1b14e8832c5e5bee4645a0913f6fdd58e2cd7b34
dev-user <no.email>
2017-05-08 18:08:59 -0400

    Ship still incomplete, but is now rendering based on sprite blocks -- essentially a sprite sheet

--- Ship.cpp
+++ Ship.cpp
@@ -1,15 +1,40 @@
 #include "Ship.h"
 #include "Textures.h"
+#include <vector>
 
+//usings 
+using std::vector;
+using std::make_shared;
+using std::shared_ptr;
 
+//foward declarations
+void populateComponents(std::vector<sf::Sprite>& container, const sf::Texture& texture, int dimension);
+short getPosKey(char x, char y);
 
 ee::Ship::Ship() : Actor()
 {
 	auto textureHolder = Textures::getInstance();
 	auto texture = textureHolder->getPlankSheet();
 	this->temporarySprite = std::make_shared<sf::Sprite>(*texture);
-}
 
+	vector<sf::Sprite> blocksToUse;
+	populateComponents(blocksToUse, *texture, 32);
+
+	//create the components of the ship
+	for (char i = 0; i < widthBlocks; ++i) {
+		for (char j = 0; j < heightBlocks; ++j) {
+			//TODO randomly pick a plank texture (currently only using 3rd)
+			sf::Sprite block = blocksToUse[3];
+
+			block.setPosition(i * 32, j * 32);
+
+			//insert a copied sprite based on the block used
+			this->components[getPosKey(i, j)] = make_shared<sf::Sprite>(block);
+		}
+	}
+
+
+}
 
 ee::Ship::~Ship()
 {
@@ -17,7 +42,40 @@ ee::Ship::~Ship()
 
 void ee::Ship::draw(sf::RenderWindow & window) const
 {
+	//for debugging
 	if (temporarySprite) {
-		window.draw(*temporarySprite);
+		//window.draw(*temporarySprite);
 	}
+
+	//iterate over the entire components and draw, STL gaurantees iterators are O(n)
+	for (auto it : this->components) {
+		window.draw(*(it.second));
+	}
+
+
+}
+
+
+void populateComponents(std::vector<sf::Sprite>& container, const sf::Texture& texture, int dimension) {
+	//create a sprite for every block in the sheet
+	for (int i = 0; i < 12; ++i) {
+		sf::Sprite temp(texture, sf::IntRect(0, i * dimension, dimension, dimension));
+
+		//make a copy into the container
+		container.push_back(temp);
+
+	}
+}
+
+/** puts two characters into a short via bit shifting */
+short getPosKey(char x, char y) {
+	short key = x;
+
+	//shift over the size of char (should be half the size of short on most platforms)
+	key <<= sizeof(char) * 8;
+
+	//OR in the bottom 8 bits of y value (x should now be displaced enough)
+	key |= y;
+	
+	return key;
 }
--- Ship.h
+++ Ship.h
@@ -1,14 +1,19 @@
 #pragma once
 #include"Actor.h"
 #include<memory>
+#include<unordered_map>
 #include<SFML/Graphics.hpp>
 
 using std::shared_ptr;
+using std::unordered_map;
 
 namespace ee {
 	class Ship : public Actor
 	{
+		unordered_map<short, shared_ptr<sf::Sprite>> components;
 		shared_ptr<sf::Sprite> temporarySprite;
+		char const widthBlocks = 5;
+		char const heightBlocks = 10;
 	public:
 		Ship();
 		~Ship();
